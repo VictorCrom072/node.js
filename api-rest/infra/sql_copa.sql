08a7b7049fe24d6117108ffac8f5c6bac141fbcc
VictorCrom072 <portovictor072@gmail.com>
2024-01-26 22:47:30 -0300

    criando conexão com banco de dados

---
+++ api-rest/infra/sql_copa.sql
@@ -0,0 +1,3 @@
+drop database if exists bd_copa;
+create database bd_copa;
+use bd_copa;
--- api-rest/server.js
+++ api-rest/server.js
@@ -1,7 +1,18 @@
 import app from './src/app.js'
+import conexao from './infra/conexao.js'
 const port = 3000
 
-//escutar(listen) a porta 3000
-app.listen(port, () => {
-    console.log(`Servidor sendo executado a partir do endereço http://localhost:${port}`)
-  })
+//executando a conexão e exibindo resultados
+conexao.connect((erro) => {
+  if(erro){
+    console.log(erro)
+  }
+  else{
+    console.log("Conexão executada com sucesso!")
+    //escutar(listen) a porta 3000
+    app.listen(port, () => {
+      console.log(`Servidor sendo executado a partir do endereço http://localhost:${port}`)
+    })
+  }
+})
+
